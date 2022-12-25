592a72aa1b127cf1d800807f74c288ba687ad055
khaja7799 <120366138+khaja7799@users.noreply.github.com>
2022-12-25 21:42:07 +0530

    Create hello24.py

---
+++ hello24.py
@@ -0,0 +1,23 @@
+def my_function():
+    a="khaja "
+    b="dudekula"
+    c=b+"  "+a
+    print(c)
+my_function()
+
+
+def my_function(food):
+    food=["apple","banana","cherry"]
+    for x in food:
+        print(x)
+my_function(food=0)
+
+
+def my_function(x):
+    return 5*x
+print(my_function(4))
+print(my_function(8))
+print(my_function(0))
+
+def my_function():
+    pass
