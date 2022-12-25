6d54606afc6a505374a0ba1ec4a5ce0c2c35dba2
khaja7799 <120366138+khaja7799@users.noreply.github.com>
2022-12-25 21:42:01 +0530

    Create hello23.py

---
+++ hello23.py
@@ -0,0 +1,12 @@
+def my_function(*kids):
+    print("the youngest childis"+kids[2])
+my_function("emil","tobias","linus")
+
+
+
+def my_function():
+    a="khaja "
+    b="dudekula"
+    c=a+" "+b
+    print(c)
+my_function
